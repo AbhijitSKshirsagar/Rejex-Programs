c058a87f55c8a05bb60d35806a1caf3d97869f85
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-03-13 01:08:49 +0530

    added: adding Last name

--- src/com/bridgelabz/regex.java
+++ src/com/bridgelabz/regex.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 public class regex {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to rejex programs");
 		UserRegistration userRegistration = new UserRegistration();
 		userRegistration.firstName();
+		userRegistration.lastName();
 
 	}
 }
@@ -16,15 +16,30 @@ public class regex {
 class UserRegistration {
 	Scanner sc;
 
+//User's First Name
 	public void firstName() {
 		sc = new Scanner(System.in);
 		System.out.print("Enter First Name :: ");
 		String FirstName = sc.next();
-		boolean answer = Pattern.matches("[A-Z]+([a-z]+)*{3,}", FirstName);
+		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", FirstName);
 		if (answer) {
 			System.out.println("thank you");
 		} else {
 			System.out.println("please enter valid first name ");
+		}
+
+	}
+
+//User's Last Name 
+	public void lastName() {
+		sc = new Scanner(System.in);
+		System.out.print("Enter Last Name :: ");
+		String LastName = sc.next();
+		Boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", LastName);
+		if (answer) {
+			System.out.println("thank you");
+		} else {
+			System.out.println("please enter valid last name ");
 		}
 
 	}
