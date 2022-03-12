88024e5b361b4afc9b8e90c45ca099b251ed9614
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-03-13 01:05:11 +0530

    added: adding first name

--- src/com/bridgelabz/regex.java
+++ src/com/bridgelabz/regex.java
@@ -1,8 +1,31 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class regex {
+
 	public static void main(String[] args) {
 		System.out.println("welcome to rejex programs");
+		UserRegistration userRegistration = new UserRegistration();
+		userRegistration.firstName();
+
+	}
+}
+
+class UserRegistration {
+	Scanner sc;
+
+	public void firstName() {
+		sc = new Scanner(System.in);
+		System.out.print("Enter First Name :: ");
+		String FirstName = sc.next();
+		boolean answer = Pattern.matches("[A-Z]+([a-z]+)*{3,}", FirstName);
+		if (answer) {
+			System.out.println("thank you");
+		} else {
+			System.out.println("please enter valid first name ");
+		}
 
 	}
 
